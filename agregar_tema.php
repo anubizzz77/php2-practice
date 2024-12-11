b5b1f57b0c17f967f6aafa01c47c5d35703328ea
Diegoneitorex <diegoarf2448@gmail.com>
2024-12-11 13:27:51 -0400

    Feat: changes

---
+++ agregar_tema.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Documento sin titulo</title>
+</head>
+
+
+
+<body>
+  <?php include "menu.php;?>"
+  <br>
+  <br>
+  <form method="post" action="agregar_tema2.php" >
+  <table align = "center" border =1>
+  <tr>
+  <td><label for="titulo">Titulo:</label></td>
+  <td input type="text" name="titulo"  id="titulo"></td>
+  </tr>
+
+  
+</body>
+</html>
---
+++ conexion.php
@@ -0,0 +1,3 @@
+<?php 
+$conex=mysqli_connect("localhost","root","","php2_foro" )or die("No se pudo conectar");
+?>
